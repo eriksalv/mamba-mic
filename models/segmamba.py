59d439005392d947c2252421f0e3ef71405af374
Jesper Elverum <jesper240800@gmail.com>
2024-10-21 12:13:20 +0200

    Fixed error in validation step

--- models/segmamba.py
+++ models/segmamba.py
@@ -374,16 +374,20 @@ class SegMambaModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         y_hat, y = self.infer_batch(batch)
         loss = self.criterion(y_hat, y)
-        self.log("train/loss", loss, prog_bar=True)
+        self.log("train_loss", loss, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        y_hat, y = self.infer_batch(batch)
+        y_hat, y = self.infer_batch(batch, val=True)
         loss = self.criterion(y_hat, y)
+
         dice_score, _ = self.dice_metric(y_hat, y)
-        self.log("val/loss", loss)
-        self.log("val/dice", dice_score.mean())
-        return loss
+        val_dice = dice_score.mean()
+
+        self.log("val_loss", loss, sync_dist=True)
+        self.log("val_dice", val_dice, sync_dist=True)
+
+        return {"val_loss": loss, "val_dice": val_dice}
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
