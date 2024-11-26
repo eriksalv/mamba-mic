811e12f89e9bfe916a4b234a80db8626a28e5cac
Erik Salvesen <eriksalvesen01@gmail.com>
2024-11-26 07:11:49 +0100

    change midRT setup, add hnts-mrg inference notebook

--- data_modules/hnts_mrg.py
+++ data_modules/hnts_mrg.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader, random_split
 from glob import glob
 import os
 from typing import Literal
+import torch
 
 
 class HNTSMRGDataModule(pl.LightningDataModule):
@@ -29,10 +30,8 @@ class HNTSMRGDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
         self.cache_rate = cache_rate
 
-        self.preprocess = (
-            preprocess
-            if preprocess is not None
-            else T.Compose(
+        default_preprocess = (
+            T.Compose(
                 [
                     T.LoadImaged(keys=["image", "label"]),
                     T.EnsureChannelFirstd(keys=["image", "label"]),
@@ -45,7 +44,30 @@ class HNTSMRGDataModule(pl.LightningDataModule):
                     T.Resized(keys=["image", "label"], spatial_size=[512, 512, 124]),
                     T.AsDiscreted(keys="label", to_onehot=3),
                 ]
+            ) if task == 'preRT'
+            else T.Compose(
+                [
+                    T.LoadImaged(keys=["t2", "t2_registered", "mask_registered", "label"]),
+                    T.EnsureChannelFirstd(keys=["t2", "t2_registered", "mask_registered", "label"]),
+                    T.ConcatItemsd(keys=["t2", "t2_registered"], name="image"),
+                    T.SelectItemsd(keys=["image", "mask_registered", "label"]),
+                    T.Orientationd(keys=["image", "mask_registered", "label"], axcodes="RAS"),
+                    T.Spacingd(
+                        keys=["image", "mask_registered", "label"],
+                        pixdim=(0.5, 0.5, 1.2),
+                        mode=("bilinear", "nearest", "nearest"),
+                    ),
+                    T.Resized(keys=["image", "mask_registered", "label"], spatial_size=[512, 512, 124]),
+                    ConvertToMultiChannelBasedOnHNTSMRGClassesd(keys="mask_registered"),
+                    T.AsDiscreted(keys="label", to_onehot=3),
+                ]
             )
+        )
+
+        self.preprocess = (
+            preprocess
+            if preprocess is not None
+            else default_preprocess
         )
         self.augment = (
             augment
@@ -74,6 +96,18 @@ class HNTSMRGDataModule(pl.LightningDataModule):
             )
         )
 
+        self.val_transform = [
+                        self.preprocess,
+                        T.NormalizeIntensityd(
+                            keys="image", nonzero=True, channel_wise=True
+                        ),
+                    ]
+        
+        if task == 'midRT':
+            self.val_transform.append(T.ConcatItemsd(keys=["image", "mask_registered"], name="image"))
+        
+        self.val_transform = T.Compose(self.val_transform)
+
         self.train_subjects = None
         self.test_subjects = None
 
@@ -88,7 +122,6 @@ class HNTSMRGDataModule(pl.LightningDataModule):
         ):
             raise FileNotFoundError(f"No train and test set found in {self.data_dir}")
 
-        # TODO: consider adding registered preRT if task is midRT
         train_t2_paths = sorted(
             glob(os.path.join(self.data_dir, "train", "*", self.task, "*_T2.nii.gz"))
         )
@@ -125,12 +158,26 @@ class HNTSMRGDataModule(pl.LightningDataModule):
             test_mask_registered_paths = sorted(
                 glob(os.path.join(self.data_dir, "test", "*", self.task, "*_mask_registered.nii.gz"))
             )
+            train_t2_pre_paths = sorted(
+                glob(os.path.join(self.data_dir, "train", "*", "preRT", "*_T2.nii.gz"))
+            )
+            test_t2_pre_paths = sorted(
+                glob(os.path.join(self.data_dir, "test", "*", "preRT", "*_T2.nii.gz"))
+            )
+            train_mask_pre_paths = sorted(
+                glob(os.path.join(self.data_dir, "train", "*", "preRT", "*_mask.nii.gz"))
+            )
+            test_mask_pre_paths = sorted(
+                glob(os.path.join(self.data_dir, "test", "*", "preRT", "*_mask.nii.gz"))
+            )
 
-            for t2, mask, t2_registered, mask_registered in zip(
+            for t2, mask, t2_registered, mask_registered, t2_pre, mask_pre in zip(
                 train_t2_paths,
                 train_mask_paths,
                 train_t2_registered_paths,
                 train_mask_registered_paths,
+                train_t2_pre_paths,
+                train_mask_pre_paths,
                 strict=True,
             ):
                 self.train_subjects.append(
@@ -139,14 +186,18 @@ class HNTSMRGDataModule(pl.LightningDataModule):
                         "label": mask,
                         "t2_registered": t2_registered,
                         "mask_registered": mask_registered,
+                        "t2_pre": t2_pre,
+                        "mask_pre": mask_pre
                     }
                 )
 
-            for t2, mask, t2_registered, mask_registered in zip(
+            for t2, mask, t2_registered, mask_registered, t2_pre, mask_pre in zip(
                 test_t2_paths,
                 test_mask_paths,
                 test_t2_registered_paths,
                 test_mask_registered_paths,
+                test_t2_pre_paths,
+                test_mask_pre_paths,
                 strict=True,
             ):
                 self.test_subjects.append(
@@ -155,6 +206,8 @@ class HNTSMRGDataModule(pl.LightningDataModule):
                         "label": mask,
                         "t2_registered": t2_registered,
                         "mask_registered": mask_registered,
+                        "t2_pre": t2_pre,
+                        "mask_pre": mask_pre
                     }
                 )
 
@@ -176,14 +229,7 @@ class HNTSMRGDataModule(pl.LightningDataModule):
             )
             self.val_set = CacheDataset(
                 val_subjects, 
-                transform=T.Compose(
-                    [
-                        self.preprocess,
-                        T.NormalizeIntensityd(
-                            keys="image", nonzero=True, channel_wise=True
-                        ),
-                    ]
-                ),
+                transform=self.val_transform,
                 cache_rate=0
             )
 
@@ -191,14 +237,7 @@ class HNTSMRGDataModule(pl.LightningDataModule):
         if stage == "test" or stage is None:
             self.test_set = CacheDataset(
                 self.test_subjects,
-                transform=T.Compose(
-                    [
-                        self.preprocess,
-                        T.NormalizeIntensityd(
-                            keys="image", nonzero=True, channel_wise=True
-                        ),
-                    ]
-                ),
+                transform=self.val_transform,
                 cache_rate=0.0,
             )
 
@@ -220,3 +259,14 @@ class HNTSMRGDataModule(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_set, batch_size=self.batch_size)
+
+class ConvertToMultiChannelBasedOnHNTSMRGClassesd(T.MapTransform):
+    def __call__(self, data):
+        d = dict(data)
+        for key in self.keys:
+            if key in data:
+                result = []
+                result.append(d[key] == 1)
+                result.append(d[key] == 2)
+                d[key] = torch.stack(result, axis=0).squeeze().float()
+        return d
